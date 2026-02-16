b0e5efbd7c20e13cd6405e4234798582f7e5b5dc
jeevitha <jeevitharamesh0123.com>
2026-02-16 16:18:07 +0100

    Updated entire project

--- 4_Arrays.js
+++ 4_Arrays.js
@@ -1,9 +1,31 @@
 let array_nums = [1, 2, 3, 4, 5];
 
-let square = (num) => {
-    return num * num;
-};
-let squared_num = array_nums.map(square);
-
+// let square = (num) => {
+//     return num * num;
+// };
+// let squared_num = array_nums.map(square);
 let squared_num_correct = array_nums.map((num) => num * num);
-console. log(squared_num);
+// console. log(squared_num);
+
+//Filter
+let age = [1, 5, 34, 78, 87, 29, 20];
+
+let filtered_Age = age.filter((age) =>{ 
+    if (age >= 18){
+    return age;
+}
+});
+// console.log(filtered_Age);
+
+//Reduce
+let arraySpend_Februray = [
+  { item: "Grocery", price: 25 },
+  { item: "Clothing", price: 100 },
+  { item: "Mobile", price: 250 },
+  { item: "Trip", price: 500 },
+];
+let sum = arraySpend_Februray.reduce((prev, curr) => {
+    console.log(prev, "+", curr);
+    return prev + curr.price;
+}, 0);
+console.log(sum);
---
+++ 5_PrimvsRef.js
@@ -0,0 +1,16 @@
+let age = 25;
+let newAge = age;
+
+newAge = newAge + 1;
+console.log(age, newAge);
+
+
+//primitive datatype
+
+let person ={
+    name:"John",
+    age: 25,
+};
+let newPerson = person;
+newPerson.age = 26;
+console.log(person);
